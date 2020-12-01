8470215f346d2b8e4963d10a9b7f296f8a1c073a
Maxim Kamalov <MaximKamalov@outlook.com>
2020-12-01 22:09:52 +0400

    ⚡️ somewhat optimized 2 biggest bottlenecks:
    canvas texture pulling and lighting

--- Assets/Scripts/P5Exhibit.cs
+++ Assets/Scripts/P5Exhibit.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.Profiling;
 #if UNITY_WEBGL && !UNITY_EDITOR
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
@@ -7,7 +8,7 @@ using System.Runtime.InteropServices;
 namespace GenCExpo
 {
     [RequireComponent(typeof(Renderer))]
-    public class P5Exhibit: MonoBehaviour
+    public class P5Exhibit : MonoBehaviour
     {
         private const byte PlaqueDistance = 4;
 
@@ -56,6 +57,11 @@ namespace GenCExpo
         private Texture2D _texture;
         private float _timer;
 
+        private bool _isVisible = true;
+
+        private void OnBecameVisible() => _isVisible = true;
+        private void OnBecameInvisible() => _isVisible = false;
+
         private void Start()
         {
             if (string.IsNullOrEmpty(_name))
@@ -67,6 +73,14 @@ namespace GenCExpo
             rend.materials[_materialIndex].mainTexture = _texture;
         }
 
+        private void ShowPlaqueIfAimedAt()
+        {
+            Profiler.BeginSample(nameof(ShowPlaqueIfAimedAt));
+            var ray = Camera.main.ViewportPointToRay(Vector3.one * .5f);
+            _plaque.SetActive(GetComponent<Collider>().Raycast(ray, out _, PlaqueDistance));
+            Profiler.EndSample();
+        }
+
         private void Update()
         {
             if (_plaque)
@@ -75,32 +89,44 @@ namespace GenCExpo
             if (string.IsNullOrEmpty(_name))
                 return;
 
-            if (InitP5Instance(_name))
+            Profiler.BeginSample(nameof(InitP5Instance));
+            var ready = InitP5Instance(_name);
+            Profiler.EndSample();
+
+            if (!ready || !_isVisible)
+                return;
+
+            Profiler.BeginSample("Get width and height");
+            var width = GetP5CanvasTextureWidth(name);
+            var height = GetP5CanvasTextureHeight(name);
+            Profiler.EndSample();
+
+            if (width != _texture.width || height != _texture.height)
             {
-                var width = GetP5CanvasTextureWidth(name);
-                var height = GetP5CanvasTextureHeight(name);
-
-                if (width != _texture.width || height != _texture.height)
-                {
-                    _texture.Resize(width, height, TextureFormat.ARGB32, false);
-                    _texture.Apply();
-                }
-
-                GetP5CanvasTexture(_name, _texture.GetNativeTexturePtr().ToInt32());
-
-                if (_restartSeconds > 0 && _timer > _restartSeconds)
-                {
-                    RecreateP5Instance(_name);
-                    _timer = 0;
-                }
-                _timer += Time.deltaTime;
+                Profiler.BeginSample(nameof(Texture2D.Resize));
+                _texture.Resize(width, height, TextureFormat.ARGB32, false);
+                _texture.Apply();
+                Profiler.EndSample();
             }
-        }
 
-        private void ShowPlaqueIfAimedAt()
-        {
-            var ray = Camera.main.ViewportPointToRay(Vector3.one * .5f);
-            _plaque.SetActive(GetComponent<Collider>().Raycast(ray, out _, PlaqueDistance));
+            Profiler.BeginSample("Get native texture pointer");
+            var ptr = _texture.GetNativeTexturePtr().ToInt32();
+            Profiler.EndSample();
+
+            Profiler.BeginSample(nameof(GetP5CanvasTexture));
+            GetP5CanvasTexture(_name, ptr);
+            Profiler.EndSample();
+
+            if (_restartSeconds > 0 && _timer > _restartSeconds)
+            {
+                Profiler.BeginSample(nameof(RecreateP5Instance));
+                RecreateP5Instance(_name);
+                Profiler.EndSample();
+
+                _timer = 0;
+            }
+
+            _timer += Time.deltaTime;
         }
     }
 }
