8789c86030167dbb59535caeedd46900712cf6ad
Maxim Kamalov <dogfuntom@users.noreply.github.com>
2021-01-09 16:01:00 +0400

    ⚗ mobile support. However, I can't test it fully, so it's tested only with simulation flags (#20)

---
+++ Assets/Scripts/DisableMouseLockOnMobile.cs
@@ -0,0 +1,22 @@
+﻿using TheFirstPerson;
+using UnityEngine;
+
+[RequireComponent(typeof(FPSController))]
+internal sealed class DisableMouseLockOnMobile : MonoBehaviour
+{
+    public bool DisableMouseLookCompletely = true;
+    public bool Simulate;
+
+    private void Awake()
+    {
+        if (!Simulate && !Application.isMobilePlatform)
+            return;
+
+        var fpsc = GetComponent<FPSController>();
+        fpsc.mouseLockToggleEnabled = false;
+        fpsc.startMouseLock = false;
+
+        if (DisableMouseLookCompletely)
+            fpsc.mouseLookEnabled = false;
+    }
+}
